55b76f03a6a33156f6d195a96e9d7370090c7170
NexusErexus <uksus924@gmail.com>
2025-07-19 16:59:48 +0300

    weapon pickup rotating

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -3,7 +3,26 @@ using UnityEngine;
 public class WeaponPickup : MonoBehaviour
 {
     [SerializeField] WeaponSO weaponSO;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float positionPeriod = 10f;
+    [SerializeField] float rotationPeriod = 30f;
     
+    Vector3 startingPosition;
+    
+    const float TAU_VALUE = Mathf.PI * 2;
+    float movementFactor;
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    public void Update()
+    {
+
+        MoveWeapon();
+        RotateWeapon();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -12,5 +31,20 @@ public class WeaponPickup : MonoBehaviour
             Destroy(this.gameObject);
             activeWeapon.SwitchWeapon(weaponSO);
         }
+    }
+
+    public void MoveWeapon()
+    {
+        if (positionPeriod <= Mathf.Epsilon) return;
+        
+        float cycles = Time.time / positionPeriod;
+        float RawSinWave = Mathf.Sin(cycles * TAU_VALUE);
+        movementFactor = (RawSinWave + 1f) / 2f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+    public void RotateWeapon()
+    {
+        transform.Rotate(rotationPeriod * Time.deltaTime * Vector3.up);
     }
 }
